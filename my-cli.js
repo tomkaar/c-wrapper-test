86462bdec922f0cba1fdae3f840bc6bbd0d54015
Tommy Kärnström <tommy.karnstrom@weahead.se>
2019-10-01 18:01:15 +0200

    test

--- my-cli.js
+++ my-cli.js
@@ -4,8 +4,16 @@
 const path = require('path');
 const bootstrap = require('commitizen/dist/cli/git-cz').bootstrap;
 
+const p = path.join(__dirname, '../node_modules/commitizen')
+
+console.log('__dirname');
+console.log(__dirname);
+
+console.log('p');
+console.log(p);
+
 bootstrap({
-  cliPath: path.join(__dirname, '../node_modules/commitizen'),
+  cliPath: p,
   // this is new
   config: {
     "path": "cz-conventional-changelog"
