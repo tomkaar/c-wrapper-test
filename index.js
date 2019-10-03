9d80b93982f85753dc9ae149201069cfc8de5dfe
Tommy Kärnström <tommy.karnstrom@weahead.se>
2019-10-03 07:52:23 +0200

    feat(repo): update stuff
    update stuff

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
-"use strict";
+'use strict';
 
 const path = require('path');
 const bootstrap = require('commitizen/dist/cli/git-cz').bootstrap;
 
 bootstrap({
-  cliPath: path.join(__dirname, 'node_modules/commitizen'),
+  cliPath: path.join(__dirname, '../../node_modules/commitizen'),
   // this is new
   config: {
-    "path": "cz-conventional-changelog"
-  }
+    path: 'cz-conventional-changelog',
+  },
 });
